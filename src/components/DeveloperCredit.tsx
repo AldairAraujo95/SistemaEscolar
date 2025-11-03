1c28f3e632966b990ab4ae940a69a1ea3e7639e4
[dyad] <aldairaraujob13@gmail.com>
2025-11-02 23:25:02 -0300

    [dyad] Adicionando link do LinkedIn - wrote 1 file(s)

--- src/components/DeveloperCredit.tsx
+++ src/components/DeveloperCredit.tsx
@@ -1,7 +1,17 @@
 export const DeveloperCredit = () => {
   return (
     <div className="p-4 text-center text-xs text-gray-500 dark:text-gray-400">
-      <p>Desenvolvido por Aldair Araujo</p>
+      <p>
+        Desenvolvido por{" "}
+        <a
+          href="https://www.linkedin.com/in/aldairaraujo95/"
+          className="hover:underline font-semibold"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Aldair Araújo
+        </a>
+      </p>
       <a
         href="mailto:aldairaraujob13@gmail.com"
         className="hover:underline"
